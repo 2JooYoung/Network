c2ff9eac98c5f028bed87018d3b61b84e43c181d
2JooYoung <168556569+2JooYoung@users.noreply.github.com>
2026-05-08 16:00:32 +0900

    Packet 사이즈

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,75 +1,165 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <iostream>
 #include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#include "Packet.h"
+
 #pragma comment(lib, "ws2_32")
 
-using namespace std;
+
+const char Operators[5] = { '+', '-', '*', '/', '%' };
+
 
 int main()
 {
-	//ws2_32.dll 로딩, winsock -> bsd socket 윈도우에서 구현체
-	WSAData  wsaData;
+	srand(static_cast<unsigned int>(time(nullptr)));
 
-	int Result = WSAStartup(MAKEWORD(2, 2), &wsaData);
-
-	if (Result != 0)
-	{
-		cout << "WSAStartup Error " << WSAGetLastError() << endl;
-		exit(-1);
-	}
+	WSAData wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
 	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	if (ServerSocket == INVALID_SOCKET)
-	{
-		cout << "socket Error " << WSAGetLastError() << endl;
-		exit(-1);
-	}
-
 	SOCKADDR_IN ServerSockAddr;
-	memset(&ServerSockAddr, 0, sizeof(ServerSockAddr));
+	ZeroMemory(&ServerSockAddr, sizeof(ServerSockAddr));
 	ServerSockAddr.sin_family = AF_INET;
-	ServerSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); //설정
-	ServerSockAddr.sin_port = htons(1234);
+	inet_pton(AF_INET, "127.0.0.1", (PVOID)&ServerSockAddr.sin_addr.s_addr);
+	ServerSockAddr.sin_port = htons(31000);
 
-	//blocking
-	Result = connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
-	if (Result == SOCKET_ERROR)
+	connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
+
+	while (true)
 	{
-		cout << "bind Error " << WSAGetLastError() << endl;
-		exit(-1);
+		int FirstNumber = rand() % (RAND_MAX - 1)  + 1;
+		int SecondNumber = rand() % (RAND_MAX - 1)  + 1;
+		unsigned short OperatorIndex = rand() % 5;
+
+		printf("%d%c%d", FirstNumber, Operators[OperatorIndex], SecondNumber);
+
+		//size code  first   second
+		//[][] [][] [][][][] [][][][]
+		PacketHeader Header;
+		Header.Size = sizeof(int) + sizeof(int);
+		Header.Code = OperatorIndex;
+
+
+
+		//byte order : host to network
+		Header.Size = htons(Header.Size);
+		Header.Code = htons(Header.Code);
+
+		//Header보낸다. 4
+		int WantSendBytes = sizeof(Header);
+		int SentBytes = 0;
+		int TotalSentBytes = 0;
+
+		do
+		{
+			SentBytes = send(ServerSocket, (char*)(&Header) + TotalSentBytes, WantSendBytes - TotalSentBytes, 0);
+			if (SentBytes == 0)
+			{
+				printf("connection close");
+				exit(-1);
+			}
+			else if (SentBytes < 0)
+			{
+				printf("send error");
+				exit(-1);
+			}
+			TotalSentBytes += SentBytes;
+		} while (TotalSentBytes < WantSendBytes);
+
+
+		//Data
+		//[][][][] 
+		char Data[1024] = { 0, };
+		int DataCursor = 0;
+		int Temp = htonl(FirstNumber);
+		memcpy(&Data[DataCursor], &Temp, sizeof(int));
+		DataCursor += sizeof(int);
+
+		//[][][][] [][][][]
+		Temp = htonl(SecondNumber);
+		memcpy(&Data[DataCursor], &Temp, sizeof(int));
+		DataCursor += sizeof(int);
+
+		WantSendBytes = ntohs(Header.Size);
+		SentBytes = 0;
+		TotalSentBytes = 0;
+
+		do
+		{
+			SentBytes = send(ServerSocket, (char*)(&Data) + TotalSentBytes, WantSendBytes - TotalSentBytes, 0);
+			if (SentBytes == 0)
+			{
+				printf("connection close");
+				exit(-1);
+			}
+			else if (SentBytes < 0)
+			{
+				printf("send error");
+				exit(-1);
+			}
+			TotalSentBytes += SentBytes;
+		} while (TotalSentBytes < WantSendBytes);
+
+
+		PacketHeader RecvHeader;
+		int WantRecvBytes = sizeof(RecvHeader);
+		int RecvBytes = 0;
+		int TotalRecvBytes = 0;
+		do
+		{
+			RecvBytes = recv(ServerSocket, (char*)(&RecvHeader) + TotalRecvBytes, WantRecvBytes - TotalRecvBytes, 0);
+			if (RecvBytes == 0)
+			{
+				printf("recv connection close");
+				exit(-1);
+			}
+			else if (RecvBytes < 0)
+			{
+				printf("recv error");
+				exit(-1);
+			}
+			TotalRecvBytes += RecvBytes;
+		} while (TotalRecvBytes < WantRecvBytes);
+
+		RecvHeader.Size = ntohs(RecvHeader.Size);
+		RecvHeader.Code = ntohs(RecvHeader.Code);
+
+		if (static_cast<PacketType>(RecvHeader.Code) == PacketType::Result)
+		{
+			//Data
+			long long Result = 0;
+
+			int WantRecvBytes = RecvHeader.Size;
+			int RecvBytes = 0;
+			int TotalRecvBytes = 0;
+			PacketHeader RecvHeader = { 0, };
+			do
+			{
+				RecvBytes = recv(ServerSocket, (char*)(&Result) + TotalRecvBytes, WantRecvBytes - TotalRecvBytes, 0);
+				if (RecvBytes == 0)
+				{
+					printf("recv connection close");
+					exit(-1);
+				}
+				else if (RecvBytes < 0)
+				{
+					printf("recv error");
+					exit(-1);
+				}
+				TotalRecvBytes += RecvBytes;
+			} while (TotalRecvBytes < WantRecvBytes);
+
+			Result = ntohll(Result);
+
+			printf("=%lld\n", Result);
+		}
+
+		//RecvBytes = recv(ServerSocket, Buffer, WantRecvBytes, MSG_WAITALL);
 	}
 
-
-
-	char Buffer[1024] = "Hello World";
-	int SentLength = send(ServerSocket, Buffer, sizeof(Buffer), 0);
-	if (SentLength == 0)
-	{
-		cout << "send disconnect " << endl;
-		exit(-1);
-	}
-	else if (SentLength < 0)
-	{
-		cout << "send Error " << WSAGetLastError() << endl;
-		exit(-1);
-	}
-
-	//blocking
-	int RecvLength = recv(ServerSocket, Buffer, sizeof(Buffer), 0);
-	if (RecvLength == 0)
-	{
-		cout << "recv disconnect " << endl;
-		exit(-1);
-	}
-	else if (RecvLength < 0)
-	{
-		cout << "recv Error " << WSAGetLastError() << endl;
-		exit(-1);
-	}
-
-	cout << "server send data : " << Buffer << endl;
-
+	shutdown(ServerSocket, SD_BOTH);
 
 	closesocket(ServerSocket);
 
